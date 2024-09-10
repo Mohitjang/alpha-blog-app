ee9a38f1ea7c847728771f165794a773bf9bd48e
Mohit Jangid <mjangidjj12@gmail.com>
2024-09-10 11:23:18 +0530

    create table, model and added validations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
 	def home
-		# now this code will referes to the view folders> pages folder . home.html.erb file.
+		# now this code will referes to the view folders> pages folder . home.html.erb
 	end
 
 	def about 
